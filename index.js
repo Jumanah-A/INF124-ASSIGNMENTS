1c04144d876576757567a2493e3963e67258c69c
Jumanah Almajnouni <jumanahh@outlook.com>
2022-04-14 17:46:59 -0700

    Added the array of the products in the data file and set up the detailed products page

--- index.js
+++ index.js
@@ -1,40 +1,99 @@
-var mens_clothes;
-var womens_clothes;
+import { items } from "./data.js";
 
-fetch("https://fakestoreapi.com/products/category/men's clothing")
-            .then(res => res.json())
-            .then(json => mens_clothes = json)
-            .then(
-                fetch("https://fakestoreapi.com/products/category/women's clothing")
-                .then(resp => resp.json())
-                .then(newJson => womens_clothes = newJson)
-                .then(displayProducts)
-            )
-
+displayProducts();
 function displayProducts() {
-    let products = mens_clothes.concat(womens_clothes)
+    let products = items;
     console.log(products)
     let section = document.getElementById('products-section');
     products.forEach((product) => {
-        let product_card = document.createElement('div'); 
-        product_card.setAttribute('class', 'product')
+    let productButton = document.createElement('div');
+    productButton.setAttribute('class', 'product');
+    productButton.setAttribute('id', product.id)
 
-        let product_img = document.createElement('img');
-        product_img.setAttribute('src', product.image);
-    
-        let product_title = document.createElement('h3'); 
-        product_title.innerHTML = product.title;
+      let product_img = document.createElement('img');
+      product_img.setAttribute('src', product.image);
+      product_img.setAttribute('id', product.id);
 
-        let product_price = document.createElement('h4');
-        product_price.innerHTML = product.price;
+      let product_title = document.createElement('h3');
+      product_title.setAttribute('id', product.id);
+      product_title.innerHTML = product.title;
 
-        product_card.append(product_img);
-        product_card.append(product_title);
-        product_card.append(product_price)
-        section.appendChild(product_card)
+      let product_price = document.createElement('h4');
+      product_price.setAttribute('id', product.id)
+      product_price.innerHTML = product.price;
+
+    productButton.append(product_img);
+    productButton.append(product_title);
+    productButton.append(product_price)
+    section.appendChild(productButton)
+
     });
 }
 
-// fetch('https://api.storerestapi.com/products')
-// .then(response => response.json())
-// .then(json => console.log(json))
+function handleProductClick(event){
+  // console.log("button is pressed")
+  // console.log(event.target.id);
+  var $allProducts = document.getElementById('current-product-view');
+  $allProducts.className = "view"
+  var $currentProduct = document.getElementById('all-products-view');
+  $currentProduct.className = "view hidden"
+  displayProductDetails(event.target.id)
+}
+// function displayProductDetails(productId)
+// {
+//   console.log("hi");
+//   var current;
+//   for(var i=0;i<10;i++)
+//   {
+//     if(String(items[i].id) === String(productId))
+//     {
+//       current=items[i];
+//       break;
+//       // console.log(items[i]);
+//     }
+//   }
+//   document.querySelector('.product-price').innerHTML=current.price;
+//   document.querySelector('.product-rating').innerHTML =current.rating;
+//   document.querySelector('.product-description').innerHTML =current.description;
+//   document.querySelector('.product-name').innerHTML=current.title;
+//   document.querySelector('.product-image').setAttribute('src',current.image);
+//   console.log(productId)
+
+// }
+
+function displayProductDetails(productId) {
+  console.log("hi");
+  var current;
+  for (var i = 0; i < 10; i++) {
+    if (String(items[i].id) === String(productId)) {
+      current = items[i];
+      break;
+      // console.log(items[i]);
+    }
+  }
+  console.log(current.rating)
+  document.querySelector('.product-price').innerHTML = "Price: $"+current.price;
+  document.querySelector('.product-rating').innerHTML = "Rating: "+current.rating.rate;
+  document.querySelector('.product-description').innerHTML = "Description: "+current.description;
+  document.querySelector('.product-name').innerHTML = current.title;
+
+  document.querySelector('.product-image').setAttribute('src', current.image);
+
+}
+// function switchViews(viewToSwitchTo)
+// {
+
+// }
+
+
+
+document.getElementById('1').addEventListener('click', handleProductClick);
+document.getElementById('2').addEventListener('click', handleProductClick);
+document.getElementById('3').addEventListener('click', handleProductClick);
+document.getElementById('4').addEventListener('click', handleProductClick);
+document.getElementById('15').addEventListener('click', handleProductClick);
+document.getElementById('16').addEventListener('click', handleProductClick);
+document.getElementById('17').addEventListener('click', handleProductClick);
+document.getElementById('18').addEventListener('click', handleProductClick);
+document.getElementById('19').addEventListener('click', handleProductClick);
+document.getElementById('20').addEventListener('click', handleProductClick);
