5990e784a553161315ef5054be9e42b719252964
Elvis Lin <elvis.125.ma@gmail.com>
2022-04-29 19:56:04 -0700

    maven web project

---
+++ store/src/main/java/com/example/store/ProductServlet.java
@@ -0,0 +1,78 @@
+package com.example.store;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@WebServlet(name = "products", value = "/products")
+public class ProductServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql:// localhost:3306/" + "products", "root", "kingstoneGX911");
+            Statement stmt = con.createStatement();
+            String sql = "SELECT * FROM products";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            PrintWriter writer = resp.getWriter();
+            writer.println("<html><head><link rel='stylesheet' href='styles.css'><script src='https://kit.fontawesome.com/a904bba290.js' crossorigin='anonymous'></script></head><body>");
+            writer.println("<div id='products-section'>");
+            while(rs.next()){                  
+                
+                String id = rs.getString("id");
+                String title = rs.getString("title");
+                String price =rs.getString("price");
+                String description = rs.getString("description");
+                String category = rs.getString("category");
+                String image = rs.getString("image");
+                int rate = rs.getInt("rate");
+                String count = rs.getString("count");
+
+                writer.println("<div class='product' id='" + id + "'>");
+                writer.println("<img src='" + image + "' id='" + id + "'>");
+                writer.println("<h3 class='product-title' id='" + id + "'>" + title + "</h3>");
+                writer.println("<h4 class='product-price'>" + price + "</h4>");
+
+                for (int i=0; i<rate; i++) {
+                    writer.println("<i class='fa-solid fa-star'></i>");
+                }
+
+                for (int i=0; i<5-rate; i++) {
+                    writer.println("<i class='fa-regular fa-star'></i>");
+                }
+
+                writer.println("<span class='review-count'>  " + count + " reviews</span>");
+
+                writer.println("</div>");
+                
+            }
+            writer.println("</div>");
+            writer.println("</body> </html> ");
+        }
+        catch(ClassNotFoundException e){
+            e.printStackTrace();
+        } catch (SQLException e) {
+           
+            e.printStackTrace();
+        }
+    }
+}
+
+
---
+++ store/src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="test">HTML</div>
+<a href="test.html">wefwef</a>
+<h2>Hello World!</h2>
+<form action="./hello" method="get">
+    <button type="submit">TEST</button>
+</form>
+</body>
+</html>
